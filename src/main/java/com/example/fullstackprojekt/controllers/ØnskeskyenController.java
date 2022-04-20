5ac4fc0f823747c4caff6ca04e7ab8831f358d5c
DanielKoltze <dani64h6@stud.kea.dk>
2022-04-20 13:11:44 +0200

    added lidt mapper og udkast til index-siden

---
+++ src/main/java/com/example/fullstackprojekt/controllers/ØnskeskyenController.java
@@ -0,0 +1,11 @@
+package com.example.fullstackprojekt.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ØnskeskyenController {
+
+    public String index(){
+        return "index.html";
+    }
+}
