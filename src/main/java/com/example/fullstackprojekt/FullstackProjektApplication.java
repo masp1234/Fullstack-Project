ba2087bbb91bae27c661a4d51438847e20cdd27f
masp1234 <mart2c04@stud.kea.dk>
2022-04-20 10:56:30 +0200

    second commit

---
+++ src/main/java/com/example/fullstackprojekt/FullstackProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstackprojekt/FullstackProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
